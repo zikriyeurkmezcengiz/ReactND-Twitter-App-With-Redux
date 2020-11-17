20bfed51e3acca9241c4be5305a40a1c452b3b57
Zikriye Urkmez Cengiz <zikriye.urkmez@navlungo.com>
2020-11-17 18:14:37 +0300

    add tweets page

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { handleInitialData } from "../actions/shared";
+import Dashboard from "./Dashboard";
 
 class App extends Component {
   componentDidMount() {
     this.props.dispatch(handleInitialData());
   }
   render() {
-    return <div>Starter Code</div>;
+    return <div> {this.props.loading ? null : <Dashboard />}</div>;
   }
 }
+function mapStateToProps({ authedUser }) {
+  return {
+    loading: authedUser === null,
+  };
+}
 
-export default connect()(App);
+export default connect(mapStateToProps)(App);
