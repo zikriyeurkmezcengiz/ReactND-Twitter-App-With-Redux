8526d3028888a8c397ef1198b4bd0fd546a10a4d
Zikriye Urkmez Cengiz <zikriye.urkmez@navlungo.com>
2020-11-19 06:19:29 +0300

    add new tweet component UI

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import { handleInitialData } from "../actions/shared";
 import Dashboard from "./Dashboard";
 import LoadingBar from "react-redux-loading";
+import NewTweet from "./NewTweet";
 
 class App extends Component {
   componentDidMount() {
@@ -12,7 +13,7 @@ class App extends Component {
     return (
       <div>
         {" "}
-        <LoadingBar /> {this.props.loading ? null : <Dashboard />}
+        <LoadingBar /> {this.props.loading ? null : <NewTweet />}
       </div>
     );
   }
