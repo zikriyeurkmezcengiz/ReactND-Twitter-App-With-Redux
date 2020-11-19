d245216f708ceff09880418986cd92db98a35993
Zikriye Urkmez Cengiz <zikriye.urkmez@navlungo.com>
2020-11-19 06:40:45 +0300

    add new tweet action and reducer codes

--- src/actions/tweets.js
+++ src/actions/tweets.js
@@ -1,7 +1,30 @@
-import { saveLikeToggle } from "../utils/api";
 export const RECEIVE_TWEETS = "RECEIVE_TWEETS";
 export const TOGGLE_TWEET = "TOGGLE_TWEET";
+import { saveLikeToggle, saveTweet } from "../utils/api";
+import { showLoading, hideLoading } from "react-redux-loading";
+export const ADD_TWEET = "ADD_TWEET";
 
+function addTweet(tweet) {
+  return {
+    type: ADD_TWEET,
+    tweet,
+  };
+}
+
+export function handleAddTweet(text, replyingTo) {
+  return (dispatch, getState) => {
+    const { authedUser } = getState();
+    dispatch(showLoading());
+
+    return saveTweet({
+      text,
+      author: authedUser,
+      replyingTo,
+    })
+      .then((tweet) => dispatch(addTweet(tweet)))
+      .then(() => dispatch(hideLoading()));
+  };
+}
 export function receiveTweets(tweets) {
   return {
     type: RECEIVE_TWEETS,
--- src/components/NewTweet.js
+++ src/components/NewTweet.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { handleAddTweet } from "../actions/tweets";
 
 class NewTweet extends Component {
   state = {
@@ -15,6 +17,8 @@ class NewTweet extends Component {
     e.preventDefault();
 
     const { text } = this.state;
+    const { dispatch, id } = this.props;
+    dispatch(handleAddTweet(text, id));
 
     // todo: Add Tweet to Store
 
@@ -54,4 +58,4 @@ class NewTweet extends Component {
   }
 }
 
-export default NewTweet;
+export default connect()(NewTweet);
